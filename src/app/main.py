203c64c251c067d35dbcbcf8a6117e1eab82d883
kevin <4051421+kevits@users.noreply.github.com>
2026-09-18 23:34:48 +0200

    chore(devcontainer): update extensions and highlight settings

--- src/app/main.py
+++ src/app/main.py
@@ -6,11 +6,15 @@ logging.basicConfig(
 )
 
 log: logging.Logger = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+log.setLevel(logging.INFO)  # FIX: Fix this!
+
 
 def main() -> None:
+    """Main entry point of the application."""
+    # TODO: Main functionality goes here.
     log.info("Hello, world!")
 
 
 if __name__ == "__main__":
+    # Call main function
     main()
